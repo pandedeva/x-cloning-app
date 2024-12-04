a10a046bf4b1b6171312edc68979f369f5137b70
Pande Deva <devapande66@gmail.com>
2024-12-04 21:07:59 +0800

    add input components and update homepage

--- src/app/(others)/page.tsx
+++ src/app/(others)/page.tsx
@@ -1,3 +1,13 @@
+import InputPost from "@/components/InputPost";
+
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="min-h-screen max-w-xl mx-auto border-l border-r">
+      <div className="py-2 px-3 sticky top-0 z-50 border-b border-gray-200">
+        <h2 className="text-lg sm:text-2xl font-bold">Home</h2>
+      </div>
+
+      <InputPost />
+    </div>
+  );
 }
